336e7006e2b24ab3f8d5c9ba89d0e90033e296a6
Williamsja0314 <Williams_ja@icloud.com>
2015-03-07 14:01:49 -0800

    cleanup

--- HOME/R_PROG/rankall_test_algorithm.R
+++
@@ -1,8 +0,0 @@
-# possible algorithm for RANKALL
-for (i in seq_along(data)) {
-data2 <- lapply(data, function(x) {
-                        
-                        if (num == "worst") num <- rows[[names(data[i])]]
-                        if (num == "best") num <- 1
-                        x[num,c("hospital", "state")]
-        })}
