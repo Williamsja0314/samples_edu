6ff53bc5e2394d873ba3c305fc069235bb9dfe56
Williamsja0314 <Williams_ja@icloud.com>
2015-03-19 11:53:54 -0700

    Updated R study

--- HOME/GC_wk3_reshaping_MergingData1.R
+++ HOME/GC_wk3_reshaping_MergingData1.R
@@ -32,7 +32,7 @@ unlist(sprCount)
 sapply(spIns,sum)
 ##
 #Use plyr pkg to do in one step. Use "dot" + paren around the variable name.
-library(plyr)
+library(dplyr)
 ddply(InsectSprays,.(spray), summarize,sum=sum(count))
 ##
 #Example
@@ -42,5 +42,43 @@ dim(spraySums) # makes same dimension as the Insectspray dataset
 #will set sum for every variable in dataset, but the variable is listed the full
 #number of times that it appears in the dataset
 head(spraySums)
-
-
+####
+###
+# Using dplyr  to select and change data
+chicago <- readRDS("./data/chicago.rds")
+dim(chicago)
+str(chicago)
+names(chicago)
+head(select(chicago, city:dptp))
+# Can select "all except"
+head(select(chicago, -(city:dptp)))
+# Using a filter with dplyr
+# gives column with values specified
+chic.f <- filter(chicago, pm25tmean2 > 30)
+chic.f <- filter(chicago, pm25tmean2 > 30 & tmpd > 80)
+# Order rows according to the data variable
+chicago <- arrange(chicago, date)
+#Can rename a column
+chicago <- rename(chicago, pm25 = pm25tmean2, dewpoint = dptp)
+#
+# Create a new variable with 'mutate' function
+# pm25 variable with mean subtracted gives new 'trend' variable
+# variable ~ "deviations from the mean"
+chicago <- mutate(chicago, pm25detrend = pm25 - mean(pm25, na.rm = TRUE))
+###
+##
+# Using the group by function
+#creating a variable, indicating if day is hot or cold
+chicago <- mutate(chicago, tempcat = factor(1 * (tmpd > 80), labels = c("cold", "hot")))
+#split on temp category variable
+hotcold <- group_by(chicago, tempcat)
+#can summarize
+summarize(hotcold, pm25 = mean(pm25), o3 = max(o3tmeans), no2 = median(no2tmean2))
+#
+summarize(hotcold, pm25 = mean(pm25,na.rm = TRUE), o3 = max(o3tmeans), no2 = median(no2tmean2))
+#summary for each year
+chicage <- mutate(chicago, year = asPOSITlt(data)$year + 1900)
+years <- group_by(chicago,year)
+summarize(years, pm25 = mean(pm25,na.rm = TRUE), o3 = max(o3tmeans), no2 = median(no2tmean2))
+## Special operator to allow chain operations together '%>%'  .. a pipeline operator
+chicago %>% mutate(month = as.POSITlt(data)$mon + 1) %>% group_by(month) %>% summarize(pm25 = mean(pm25, na.rm = TRUE), o3 = max(o3tmean2), no2 = median(no2tmean2))
