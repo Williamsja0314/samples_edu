da6ca41a8fe15286aba84b0a74414e8d2a0784bc
Joe Williams <jowilliams@apptio.com>
2015-03-24 13:50:19 -0700

    updates

---
+++ HOME/GC_Quiz3_with_jpeg.R
@@ -0,0 +1,13 @@
+
+
+acsq3 <- read.csv("./data/getdata-data-ss06hid.csv", header=TRUE)
+agricultureLogical <- (acsq3$ACR == 3 & acsq3$AGS == 6)
+which(agricultureLogical)
+#
+## read in with jpeg package
+library(jpeg)
+img.n <- readJPEG(system.file("img","getdata-jeff.jpg", package="jpeg"),native=TRUE) 
+##
+GDP <- read.csv("./data/GDP.csv")
+EDU <- read.csv("./data/EDU.csv")
+mergedData <- merge(GDP,EDU,by.x="X",by.y="CountryCode",all=FALSE, na.rm=TRUE)
