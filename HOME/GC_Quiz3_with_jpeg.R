256e914bd79b26f4ff208cc64d68039be50a6bff
Williamsja0314 <Williams_ja@icloud.com>
2015-03-28 18:25:10 -0700

    updates

--- HOME/GC_Quiz3_with_jpeg.R
+++ HOME/GC_Quiz3_with_jpeg.R
@@ -9,8 +9,21 @@ file.info("getdata-jeff.jpg")
 img.n <- readJPEG("getdata-jeff.jpg",native=TRUE)
 quantile(img.n,c(0,.30,.80,1))
 ##
-GDP <- read.csv("./data/GDP.csv")
+
+gdp1 <- read.csv("./data/GDP1.csv")
 EDU <- read.csv("./data/EDU.csv")
 EDU <- subset(EDU,!is.na(EDU$CountryCode))
 mergedData <- merge(EDU,GDP,by.x= "CountryCode",by.y= "X",all=FALSE)
+#Quiz 4
 AvgOECD <- subset(GDP,GDP$Income.Group == 'High income: nonOECD')
+GDP <- read.csv("./data/getdata_data_GDP.csv")
+hidsplit <- read.csv("./data/getdata_data_ss06hid.csv")
+hidsplitn <- names(hidsplit)
+answer <- strsplit(hidsplitn, "wgtp")
+#
+library(xlsx)
+gdp190 <- read.xlsx("./data/grossproduct190.xls",sheetIndex=1,header=TRUE)
+gdp190ans <- subset(gdp190, !is.na(gdp190[5]))
+library(quantmod)
+amzn = getSymbols("AMZN",auto.assign=FALSE)
+sampleTimes = index(amzn) 
