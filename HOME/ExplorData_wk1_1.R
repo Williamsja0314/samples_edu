ceed395a7f1f14f4ce1c39da32790e7d0cea7b41
Williamsja0314 <Williams_ja@icloud.com>
2015-04-09 14:16:52 -0700

    updates for Explo

---
+++ HOME/ExplorData_wk1_1.R
@@ -0,0 +1,15 @@
+#week 1 of exploratory data analysis
+#plot based on groups of male and female
+par(mfrow = c(1,1))
+x <- rnorm(100)
+y <- x + rnorm(100)
+g <- gl(2,50)
+g <- gl(2,50, labels = c("Male","Female"))
+# information purposes only. Begin
+str(g)
+# information purposes only. End
+plot(x,y)
+plot(x,y, type = "n")
+points(x[g == "Male"], y[g == "Male"], col = "green")
+points(x[g == "Female"], y[g == "Female"], col = "blue")
+points(x[g == "Female"], y[g == "Female"], col = "blue", pch = 19)
